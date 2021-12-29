cb40c36e54b3e0437e65b88763095f56c749f961
minggu99 <tuanphamtran99@gmail.com>
2021-12-29 20:57:42 +0700

    update autoload video vemo 1

--- public/js/video.js
+++ public/js/video.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  const videos = document.getElementsByClassName("note-video-clip");
+  const videos = document.querySelectorAll("iframe");
   if(videos && videos.length > 0){
     for(let video of videos){
       if(video.src.includes("youtube")){
