3a4cde371fe1303ad0524b46e9da46025c57255f
minggu99 <tuanphamtran99@gmail.com>
2021-12-29 20:54:30 +0700

    update autoload video vemo

--- public/js/video.js
+++ public/js/video.js
@@ -2,8 +2,12 @@ $(document).ready(function () {
   const videos = document.getElementsByClassName("note-video-clip");
   if(videos && videos.length > 0){
     for(let video of videos){
-      
-      video.src += "?autoplay=1&mute=1";
+      if(video.src.includes("youtube")){
+        video.src += "?autoplay=1&mute=1";
+      }
+      else if(video.src.includes("vimeo")){
+        video.src += "?autoplay=1&loop=1&autopause=0";
+      }
     }
   }
 });
