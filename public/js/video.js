d7a8b206f2f0cbf4a0e96e02a73d65b8b4ac8fb6
minggu99 <tuanphamtran99@gmail.com>
2021-12-30 23:47:34 +0700

    fix autoplay video

--- public/js/video.js
+++ public/js/video.js
@@ -1,11 +1,13 @@
 $(document).ready(function () {
-  const videos = $("iframe");
+  const videos = document.querySelectorAll("iframe");
   if(videos && videos.length > 0){
     for(let video of videos){
       if(video.src.includes("youtube")){
-        video.src += "?autoplay=1&mute=0";
-        video.attr("frameborder", 0);
-        video.attr("allowfullscreen");
+        video.src += "?autoplay=1&loop=1&amp;mute=1";
+        video.setAttribute("frameborder", 0);
+        video.setAttribute("allowfullscreen", 1);
+        // video.attr("frameborder", 0);
+        // video.attr("allowfullscreen", 1);
       }
       else if(video.src.includes("vimeo")){
         video.src += "?autoplay=1&loop=1&autopause=0";
