bd8c16342e0f177b4cadb048c41b3c4ffa53c6ae
minggu99 <tuanphamtran99@gmail.com>
2021-12-30 21:30:52 +0700

    fix editor height

--- public/js/video.js
+++ public/js/video.js
@@ -1,9 +1,11 @@
 $(document).ready(function () {
-  const videos = document.querySelectorAll("iframe");
+  const videos = $("iframe");
   if(videos && videos.length > 0){
     for(let video of videos){
       if(video.src.includes("youtube")){
-        video.src += "?autoplay=1&mute=1";
+        video.src += "?autoplay=1&mute=0";
+        video.attr("frameborder", 0);
+        video.attr("allowfullscreen");
       }
       else if(video.src.includes("vimeo")){
         video.src += "?autoplay=1&loop=1&autopause=0";
